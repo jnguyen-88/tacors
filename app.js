c13d56737eae51d7d2361c6c4f9b3cac24517236
Justin Nguyen <justinhnguyen88@gmail.com>
2019-09-16 13:11:00 -0700

    testing heroku

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const currentUser = (req, res, next) => {
 // Mongoose Config
 //================
 // mongoose.connect('mongodb://localhost/tacors',{useNewUrlParser: true});
-mongoose.connect('mongodb+srv://tacors:tacorspassword@cluster0-a2ze8.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://tacors:tacorspassword@tacocluster-a2ze8.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true
 }).then(() => {
